4c15d033de8b3ff670c44ad41e4b8e3c8ca76e08
gabrielaperezbello <gabrielaperezbello@10.232.14.182>
2022-10-25 10:22:25 -0700

    Nuevo proyecto Catrina

---
+++ src/mx/itson/catrina/entidades/Cliente.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.catrina.entidades;
+
+/**
+ *
+ * @author gabrielaperezbello
+ */
+public class Cliente {
+    private String nombre;
+    private String rfc;
+    private String direcccion;
+    private String ciudadEstado;
+    private String codigoPostal;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String getDirecccion() {
+        return direcccion;
+    }
+
+    public void setDirecccion(String direcccion) {
+        this.direcccion = direcccion;
+    }
+
+    public String getCiudadEstado() {
+        return ciudadEstado;
+    }
+
+    public void setCiudadEstado(String ciudadEstado) {
+        this.ciudadEstado = ciudadEstado;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+}
